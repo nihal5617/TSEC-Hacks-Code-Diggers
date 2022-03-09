47b9f071a6f0afd7bb4a6a1faabf97791cb66fea
Ronnit3012 <ronnit.mirgh@gmail.com>
2022-03-09 17:23:16 +0530

    linking user profile

--- server.js
+++ server.js
@@ -29,10 +29,29 @@ app.get('', (req, res) => {   // Making it Dynamic
     });
 });
 
+// For SignUp.hbs
+app.get('/SignUp', (req, res) => {   // Making it Dynamic
+    res.render('Signup', {
+        title: 'Weather',  
+        name: 'Ronnit Mirgh'
+    });
+});
 
-const CONNECTION_URL = 'mongodb+srv://vraj:vraj123@cluster0.kbqn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+// For LogIn.hbs
+app.get('/Login', (req, res) => {   // Making it Dynamic
+    res.render('Login', {
+        title: 'Weather',  
+        name: 'Ronnit Mirgh'
+    });
+});
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
-    .catch((error) => console.log(error.message));
+app.listen(5000, () => {
+    console.log('Server is up on port 5000');
+})
+
+// const CONNECTION_URL = 'mongodb+srv://vraj:vraj123@cluster0.kbqn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+//     .catch((error) => console.log(error.message));
